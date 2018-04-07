ab563de50c558a56af1fc455ea0c239472ca386e
xinlei <weixinlei87@163.com>
2018-04-07 16:06:28 +0800

    读取json文件验证用户

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,47 @@
+'use strict';
+
 var app = angular.module('myApp',['ngRoute']);
-app.controller('login',function($scope,$location){
+app.controller('loginController',function($scope,$location,$http){
+  $scope.ok1 = true;
+  $scope.ok2 = true;
+  $scope.accountname = "";
+  $scope.password = "";
+  $scope.accounts = [];
+
   $scope.concel= function(){
-    accoutname = "";
-    password = "";
+    $scope.accountname = "";
+    $scope.password = "";
   }
   $scope.chekLogin = function(){
     //验证用户名，密码
-    $location.path("/employee");
+    if($scope.accountname == ""){
+      $scope.ok1 = false;
+      return ;
+    }
+    else if($scope.password == ""){
+      $scope.ok2 = false;
+      return ;
+    }
+    $http.get('/home/xinlei/mydapp/src/assets/account.json').then(function(response){
+      $scope.accounts = response.data;
+      for( var i = 0; i< $scope.accounts.length;i++){
+        if($scope.accounts[i].accountname == $scope.accountname){
+          if($scope.accounts[i].password == $scope.password && $scope.accounttype == $scope.accounts[i].type){
+            $location.path("/employee");
+            return;
+          }
+          else{
+            alert("密码或账户类型错误！");
+            return ;
+          }
+        }
+      }
+      alert("用户名不存在！");
+      return;
+    },function(date){
+      console.log(data);
+    })
+
   }
 })
 app.controller('employeeController',function($scope){
@@ -15,26 +50,26 @@ app.controller('employeeController',function($scope){
 app.controller('employerController',function($scope){
 
 })
-app.config(['$routeProvide','$locationProvider',function($routeProvide,$locationProvider){
+app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
   $locationProvider.hashPrefix('');
-  $routeProvide.when(
+  $routeProvider.when(
     '/employee',
     {
-      templateUrl:'../employee.html',
+      templateUrl:'/home/xinlei/mydapp/src/employee.html',
       controller:'employeeController'
     }
   )
-  $routeProvide.when(
+  $routeProvider.when(
     '/employer',
     {
-      templateUrl:'../employer.html',
+      templateUrl:'/home/xinlei/mydapp/src/employer.html',
       controller:'employerController.html'
     }
   )
-  $routeProvide.when(
+  $routeProvider.when(
     '/',
     {
-      templateUrl:'../login.html',
+      templateUrl:'/home/xinlei/mydapp/src/login.html',
       controller:'loginController'
     }
   )
